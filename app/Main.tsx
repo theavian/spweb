0f010482ae544e8e933f579b22216b461ce72e21
Akhilesh Vaghela <90500790+theavian@users.noreply.github.com>
2026-06-02 14:28:11 +0530

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,7 +9,6 @@ const MAX_DISPLAY = 2
 export default function Home({ posts }) {
   return (
     <>
-      
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">

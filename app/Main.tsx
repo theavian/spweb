1180ab1a5a2fac1d7601eadc0068ff9d128d35aa
Akhilesh Vaghela <90500790+theavian@users.noreply.github.com>
2026-06-02 13:45:12 +0530

    Patch-3

--- app/Main.tsx
+++ app/Main.tsx
@@ -4,15 +4,16 @@ import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 2
 
 export default function Home({ posts }) {
   return (
     <>
+      
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
+            What's New
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}

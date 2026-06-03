fbcc416b4a03da71ea96b3ccba24eebfda937737
Akhilesh Vaghela <90500790+theavian@users.noreply.github.com>
2026-06-03 12:54:18 +0530

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,14 +9,15 @@ const MAX_DISPLAY = 2
 export default function Home({ posts }) {
   return (
     <>
+      <section className="py-2">
         <div className="container mx-auto px-6">
-          <div className="mx-auto max-w-3xl text-center">
-            <span className="text-sm font-semibold tracking-wider text-blue-600 uppercase">
+          <div className="mx-auto max-w-3xl">
+            <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
               About Us
-            </span>
-            <h2 className="mt-4 text-4xl font-bold text-gray-900 dark:text-white">
+            </h1>
+            <span className="text-sm font-semibold uppercase tracking-wider text-blue-600">
               Australian Based Digital Design and Development Studio
-            </h2>
+            </span>
             <p className="mt-6 text-lg text-gray-600 dark:text-gray-300">
               Sproutling Studios is an indie game studio based in Australia that specializes in
               exploring the realms of visual art and animation, along with creative Minecraft
@@ -32,11 +33,12 @@ export default function Home({ posts }) {
             </div>
           </div>
         </div>
+      </section>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5 text-center">
-          <span className="text-sm font-semibold tracking-wider text-blue-600 uppercase">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             What's New
-          </span>
+          </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             The latest news on games, development updates, and major announcements.
           </p>
@@ -104,7 +106,6 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-
       {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
